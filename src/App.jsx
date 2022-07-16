8455ba1dc24afde127e15bb575ed0cf609a30595
Guotong Liao <815190191@qq.com>
2022-07-16 19:00:21 +0800

    chore: initialize the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Quotes from './dashboard/quotes';
+import Footer from './dashboard/components/Footer'
+
+function App() {
+  return (
+    <div className="App">
+      <Quotes />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
