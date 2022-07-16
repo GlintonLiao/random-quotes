166a1aacdd4d081c188822eda0f3c4e07b395a35
Guotong Liao <815190191@qq.com>
2022-07-16 20:25:31 +0800

    refractor: fix typo

--- src/dashboard/components/Footer.jsx
+++ src/dashboard/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <div id="footer">
-      <span>by</span>
+      <span>by </span>
       <a rel="noopener noreferrer" href="http://github.com/glintonliao">
         Guotong Liao
       </a>
